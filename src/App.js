cade70f1b6da11650646b4989ef2f7905da61ccf
Pritam <mail@pritam.dev>
2023-08-26 02:58:37 +0530

    hooks style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import { Grommet } from 'grommet';
 
-const darkTheme = {
-  global: {
-    colors: {
-      background: '#1E1E1E',
-      text: '#FFFFFF',
+const App = () => {
+  const darkTheme = {
+    global: {
+      colors: {
+        background: '#1E1E1E',
+        text: '#FFFFFF',
+      },
     },
-  },
-};
-
-class App extends Component {
-  render() {
-    return (
-      <Grommet theme={darkTheme} full>
-        <h1>Hello</h1>
-        Sint reprehenderit in dolore reprehenderit aliqua nisi et pariatur.
-      </Grommet>
-    );
-  }
+  };
+  return (
+    <Grommet theme={darkTheme} full>
+      <h1>Hello</h1>
+      Sint reprehenderit in dolore reprehenderit aliqua nisi et pariatur.
+    </Grommet>
+  );
 }
 
 export default App;
