8b750c5287cbcecf5a2a84ad4b77b99525993770
Pritam <mail@pritam.dev>
2023-08-26 06:44:38 +0530

    some more code

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import ToolsGrid from './containers/body/ToolsGrid';
 import ToolContainer from './containers/body/ToolContainer';
 import NavBar from './containers/NavBar/NavBar';
 import MainBody from './containers/body/MainBody';
-import MinimizableDiv from './containers/body/MinimizableDiv';
 
 const App = () => {
   return (
     <div className='app'>
-      <MinimizableDiv/>
+      <NavBar/>
       <MainBody/>
     </div>
   );
--- src/containers/body/ToolsCard.js
+++ src/containers/body/ToolsCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../../App.css';
+import './ToolsCard.css';
 
 export default function ToolsCard({tileInfo}) {
   return (
