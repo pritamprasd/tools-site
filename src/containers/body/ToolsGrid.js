5233ad49545ca855bd1c48d22f446a5bbd372a66
Pritam <mail@pritam.dev>
2023-09-11 21:17:12 +0530

    wuba luba dub dub

--- src/containers/body/ToolsGrid.js
+++ src/containers/body/ToolsGrid.js
@@ -7,11 +7,11 @@ import { getToolFromToolId } from '../../store/helpers';
 
 export default function ToolsGrid() {
   const { globalVariables, _ } = useGlobalContext();
-  const [ filteredTools, setfilteredTools] = useState(appConfig.toolsList)
+  const [filteredTools, setfilteredTools] = useState(appConfig.toolsList)
   useEffect(() => {
-    if(globalVariables.activeTags.length != 0){
+    if (globalVariables.activeTags.length !== 0) {
       setfilteredTools(
-        appConfig.toolsList.filter(t => 
+        appConfig.toolsList.filter(t =>
           t.tags.some((tag) => globalVariables.activeTags.includes(tag))
         )
       )
@@ -19,11 +19,11 @@ export default function ToolsGrid() {
       setfilteredTools(appConfig.toolsList)
     }
   }, [globalVariables])
-  
+
   return (
     <div className='toolsGrid'>
       {globalVariables.activeToolId !== "" && getToolFromToolId(globalVariables.activeToolId)}
-      {globalVariables.activeToolId === "" && filteredTools.map(t => <ToolsCard tileInfo={t} key={t.id}/>)}
+      {globalVariables.activeToolId === "" && filteredTools.map(t => <ToolsCard tileInfo={t} key={t.id} />)}
     </div>
   )
 }
