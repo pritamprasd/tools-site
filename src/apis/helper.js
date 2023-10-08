1f7f3a49b6f756e67c30b5c6fba7f9ae4c0ccd42
Pritam <mail@pritam.dev>
2023-10-08 23:19:26 +0530

    cleaned everthing

--- src/apis/helper.js
+++
@@ -1,6 +0,0 @@
-export async function checkResponse(response) {
-    if (!response.ok) {
-        throw new Error(` | ${await response.text()}`)
-    }
-    return response
-}
--- src/tools/site-settings/MiscSettings.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import '../../App.css';
-import './SettingsPage.css';
-
-export default function MiscSettings() {
-  return (
-    <div>MiscSettings</div>
-  )
-}
