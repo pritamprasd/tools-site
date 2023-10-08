ea62cfa899acad59559b0d55089fdc2b1712017a
Pritam <mail@pritam.dev>
2023-10-09 00:36:08 +0530

    pwa setup

--- notes.md
+++ notes.md
@@ -1,15 +1,9 @@
 ### setup commands:
 ```
-yarn add react react-dom @types/react @types/react-dom
-yarn add --dev webpack webpack-cli webpack-dev-server ts-loader react-scripts typescript
-```
-
-```
-yarn webpack build --mode development
-yarn webpack serve --mode development
-```
-
-```
 npm install --save-dev react-scripts typescript ts-loader webpack webpack-cli webpack-dev-server
 npm install react react-dom @types/react @types/react-dom
 ```
+
+
+http://localhost:8082/
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,36 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: 'ts-loader',
-      },
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+        }),
+        new WorkboxWebpackPlugin.GenerateSW({
+            clientsClaim: true,
+            skipWaiting: true,
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-    }),
-  ],
+    performance: {
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
 };
