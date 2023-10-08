482b32fedadd2c970e584cd98977621e5f4e76b5
Pritam <mail@pritam.dev>
2023-10-08 23:45:06 +0530

    stable webpack config

---
+++ notes.md
@@ -0,0 +1,10 @@
+### setup commands:
+```
+yarn add react react-dom @types/react @types/react-dom
+yarn add --dev webpack webpack-cli webpack-dev-server ts-loader react-scripts typescript
+```
+
+```
+yarn webpack build --mode development
+yarn webpack serve --mode development
+```
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+};
