e2a65249429d1c5235a8e9fb0b7e4c66b54d7bbd
update-rust-build-files-script <mail@pritam.dev>
2023-10-28 19:39:08 +0000

    Automated update for rust bulid files

--- src/wasm-crypto/pkg/wasm_crypto.d.ts
+++ src/wasm-crypto/pkg/wasm_crypto.d.ts
@@ -15,33 +15,3 @@ export function subtract(a: number, b: number): number;
 /**
 */
 export function greet(): void;
-
-export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
-
-export interface InitOutput {
-  readonly memory: WebAssembly.Memory;
-  readonly greet: () => void;
-  readonly subtract: (a: number, b: number) => number;
-  readonly add: (a: number, b: number) => number;
-}
-
-export type SyncInitInput = BufferSource | WebAssembly.Module;
-/**
-* Instantiates the given `module`, which can either be bytes or
-* a precompiled `WebAssembly.Module`.
-*
-* @param {SyncInitInput} module
-*
-* @returns {InitOutput}
-*/
-export function initSync(module: SyncInitInput): InitOutput;
-
-/**
-* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
-* for everything else, calls `WebAssembly.instantiate` directly.
-*
-* @param {InitInput | Promise<InitInput>} module_or_path
-*
-* @returns {Promise<InitOutput>}
-*/
-export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
